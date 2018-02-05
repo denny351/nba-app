26a2466b0fa8d94a93d4bc0cf1db4bbae498d8ac
Denny Park <denny.park93@gmail.com>
2018-02-05 12:46:29 -0800

    add carousel slider to home page

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import NewsSlider from '../widgets/NewsSlider/slider';
+
 const Home = () => {
-  return <div>HOme</div>
+  return (
+    <div>
+      <NewsSlider 
+        type="featured"
+        start={0}
+        amount={3}
+        settings={{
+          dots: false
+        }}
+      />
+    </div>
+  )
 };
 
 export default Home;
