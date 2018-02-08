9489f57cb9f3580a0116290a8298523e8b9708ca
Denny Park <denny.park93@gmail.com>
2018-02-07 20:57:07 -0800

    add dashboard page to create new article

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import VideoArticle from './components/Articles/Videos/Video/index';
 import NewsMain from './components/Articles/News/Main/index';
 import VideosMain from './components/Articles/Videos/Main/index';
 import SignIn from './components/SignIn/signIn';
+import Dashboard from './components/Dashboard/dashboard';
 
 const Routes = (props) => {
   return (
@@ -19,6 +20,7 @@ const Routes = (props) => {
         <Route path="/articles/:id" exact component={NewsArticle} />
         <Route path="/videos/:id" exact component={VideoArticle} />
         <Route path="/sign-in" exact component={SignIn} />
+        <Route path="/dashboard" exact component={Dashboard} />
       </Switch>
     </Layout>
   );
